f059a6de373e5485af8e53b06fec5687c6dd7bce
belalelmi <belalelmi@gmail.com>
2021-11-23 18:19:23 -0500

    groupBy queries

---
+++ 3_queries_group-by/total_assgn_submissions.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+INNER JOIN students ON students.id = student_id
+INNER JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
